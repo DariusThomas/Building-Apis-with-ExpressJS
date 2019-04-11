70e4c7079c4e79c0f54791fb3d1cc10985f0c3d9
Darius Thomas <dariusd.thomas@gmail.com>
2019-04-11 13:39:37 -0400

    Building API's with expressRequired section complete

---
+++ server/routes/chirps.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const chirpsStore = require('../../chirpstore')
+let router = express.Router();
+
+router.get('/:id?',(req,res)=>{
+    let id = req.params.id;
+    if(id){
+        res.json(chirpsStore.GetChirp(id));
+    }else{
+        res.json(chirpsStore.GetChirps());
+    }
+});
+
+router.post('/',(req,res)=>{
+    chirpsStore.CreateChirp(req.body);
+    res.sendStatus(200);
+})
+
+router.put('/:id',(req,res)=>{
+    let id= req.params.id
+    let chirp = req.body;
+    chirpsStore.UpdateChirp(id, chirp)
+    res.sendStatus(200)
+})
+
+router.delete('/:id',(req,res)=>{
+    let id = req.params.id;
+    chirpsStore.DeleteChirp(id)
+    res.sendStatus(200)
+})
+
+module.exports = router;
